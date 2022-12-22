8099f28b67f85b83a6188743e05d2e7ee791a50f
Lucenko Sergei <kll-01@mail.ru>
2022-12-22 15:37:12 +0500

    first_swiper

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 "use strict"
 
 new Swiper('.swiper', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+    
+  navigation: {
+    nextEl: '.s-button-next',
+    prevEl: '.s-button-prev',
+  },
+    
+  pagination: {
+    el: '.s-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+
+
 });
 
